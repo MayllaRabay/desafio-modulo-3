ba17190491ab0b10fdccc604be95cbc650e85cb8
Maylla Rabay <mayllarabay@gmail.com>
2021-08-12 23:27:26 -0300

    Controladores e endpoint cadastro de usuário criados

---
+++ back-end/src/controllers/users.js
@@ -0,0 +1,44 @@
+const connection = require('../connection');
+
+const createUser = async (req, res) => {
+  const { 
+    nome, 
+    nome_loja, 
+    email, 
+    senha 
+  } = req.body;
+
+  try {
+    const query = `INSERT INTO usuarios ( 
+      nome, 
+      nome_loja, 
+      email, 
+      senha
+    ) VALUES (
+      $1,
+      $2,
+      $3,
+      $4
+    );`
+
+    const user = await connection.query(query, [
+      nome, 
+      nome_loja, 
+      email, 
+      senha
+    ]);
+
+    if (user.rowCount === 0) {
+      return res.status(400).json('Não foi possível cadastrar o usuário.');
+    }
+
+    return res.status(200).json('Usuário cadastrado com sucesso!');
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+module.exports = {
+  createUser
+};
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const users = require('./controllers/users');
+const products = require('./controllers/products');
 
 const routes = express();
+
+routes.post('/cadastro', users.createUser);
 
 module.exports = routes;
