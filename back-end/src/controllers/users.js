a39656cd6b6346ec185b39d6ded776cf90979113
Maylla Rabay <mayllarabay@gmail.com>
2021-08-13 20:21:07 -0300

    Endpoint atualizar perfil criado

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const connection = require('../connection');
 
 const createUser = async (req, res) => {
@@ -57,11 +58,11 @@ const viewProfile = async (req, res) => {
       FROM usuarios
       WHERE id = $1;
     `
-    const { rows : user } = await connection.query(query, [1]);
+    const { rows : user } = await connection.query(query, [infoUser.id]);
     const { senha, ...profile} = user[0];
 
     return res.status(200).json(profile);
-    
+
   } catch (error) {
     return res.status(400).json(error.message);
   }
@@ -69,7 +70,52 @@ const viewProfile = async (req, res) => {
 }
 
 const updateProfile = async (req, res) => {
+  const { infoUser } = req;
+  const { 
+    nome, 
+    nome_loja, 
+    email, 
+    senha
+  } = req.body;
 
+  const queryEmail = `
+    SELECT *
+    FROM usuarios
+    WHERE email = $1
+    AND id <> $2;
+  `
+  const checkDuplicateEmail = await connection.query(queryEmail, [email, infoUser.id]);
+
+  if(checkDuplicateEmail.rowCount > 0) {
+    return res.status(400).json('Não foi possível atualizar seu perfil. O e-mail informado já está cadastrado.');
+  }
+
+  try {
+    const query = `
+      UPDATE usuarios
+      SET nome = $1,
+      nome_loja = $2,
+      email = $3,
+      senha = $4
+      WHERE id = $5;
+    `
+    const newProfile = await connection.query(query, [
+      nome, 
+      nome_loja, 
+      email, 
+      senha,
+      infoUser.id
+    ]);
+
+    if(newProfile.rowCount === 0) {
+      return res.status(400).json('Não foi possível atualizar seu perfil, tente novamente!');
+    }
+
+    return res.status(200).json('Perfil atualizado com sucesso!');
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
 }
 
 module.exports = {
