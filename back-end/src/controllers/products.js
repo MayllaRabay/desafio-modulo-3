f585371d26b38c2b4db9582ac2c6204f27456d1c
Maylla Rabay <mayllarabay@gmail.com>
2021-08-13 13:12:26 -0300

    Endpoint cadastrar produto criado

--- back-end/src/controllers/products.js
+++ back-end/src/controllers/products.js
@@ -47,7 +47,55 @@ const listProductId = async (req, res) => {
 }
 
 const createProduct = async (req, res) => {
+  const { infoUser } = req;
+  const {
+    nome,
+    estoque,
+    categoria,
+    preco,
+    descricao,
+    imagem
+  } = req.body;
 
+  try {
+    const query = `
+      INSERT INTO produtos (
+        usuario_id
+        nome,
+        estoque,
+        categoria,
+        preco,
+        descricao,
+        imagem
+      ) VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7
+      );
+    `
+    const product = await connection.query(query, [
+      infoUser.id, 
+      nome, 
+      estoque, 
+      categoria, 
+      preco, 
+      descricao, 
+      imagem
+    ]);
+
+    if(product.rowCount === 0) {
+      return res.status(400).json('Não foi possível cadastrar o produto.');
+    }
+
+    return res.status(200).json('Produto cadastrado com sucesso!');
+    
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
 }
 
 const updateProduct = async (req, res) => {
