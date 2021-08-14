dd0d2356508402244eacdee5411afb75301921bb
Maylla Rabay <mayllarabay@gmail.com>
2021-08-14 14:48:16 -0300

    Pontuação de mensagens de erro melhoradas

--- back-end/src/utils/handleUsers.js
+++ back-end/src/utils/handleUsers.js
@@ -1,18 +1,18 @@
 function handleUsers (nome, nome_loja, email, senha) {
   if(!nome) {
-    return 'O campo "nome" é obrigatorio!';
+    return "O campo 'nome' é obrigatório!";
   }
 
   if(!nome_loja) {
-    return 'O campo "nome_loja" é obrigatorio!';
+    return "O campo 'nome_loja' é obrigatorio!";
   }
 
   if(!email) {
-    return 'O campo "email" é obrigatorio!';
+    return "O campo 'email' é obrigatorio!";
   }
 
   if(!senha) {
-    return 'O campo "senha" é obrigatorio!';
+    return "O campo 'senha' é obrigatorio!";
   }
 }
 
